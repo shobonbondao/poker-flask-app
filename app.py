177955d3b8e6e6d922a9072a5dbb68f09fdf7444
shobonbondao <madawo.sonic@gmail.com>
2025-06-08 12:36:49 +0900

    fix port binding for render

--- app.py
+++ app.py
@@ -38,4 +38,7 @@ def calculate():
     return jsonify({'winrate': round(wins / sims * 100, 2)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
